dfad39d5e02f1185d6aef668beeeeba6200b6c3e
tammysuryana <tammysuryana@gmail.com>
2020-07-08 18:27:00 +0700

    menyalin prosess penyimpanan

---
+++ precache-manifest.62cfec9b3fdc8f096a9f8a278855f62a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41065cbce41348e220904107dcd981b",
+    "url": "/menurufu/index.html"
+  },
+  {
+    "revision": "461e5b3103a2c0510ed8",
+    "url": "/menurufu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c15f6e8f54d845e77291",
+    "url": "/menurufu/static/js/2.116eabdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menurufu/static/js/2.116eabdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461e5b3103a2c0510ed8",
+    "url": "/menurufu/static/js/main.279fa015.chunk.js"
+  },
+  {
+    "revision": "7ae2a6aefdd7e43f034e",
+    "url": "/menurufu/static/js/runtime-main.14a3588c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/menurufu/static/media/logo.5d5d9eef.svg"
+  }
+]);
